e1a0fc38fc1d948ab3824b38070cbcc20c50ec94
MatteoSquarz <matteo.squarzoni03@gmail.com>
2022-03-27 21:13:17 +0200

    ,

--- Database.py
+++ Database.py
@@ -1,2 +1,24 @@
 #Database
+import psycopg2
+
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="admin",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="prova")
+    cursor = connection.cursor()
+
+    postgreSQL_select_Query = "insert into film values (2, '2001: Odissea nello spazio', 'Stanley Kubrick', '1968-04-04');"    #immettere query qui
+    cursor.execute(postgreSQL_select_Query)
+
+    connection.commit()     #conferma e salva modifiche sul db
     
+except (Exception, psycopg2.Error) as error:
+    print("Error while fetching data from PostgreSQL", error)
+finally:
+    # closing database connection.
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
