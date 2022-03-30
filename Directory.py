bcb638462d556155821856ebd71382f299bf91b6
MatteoSquarz <93669086+MatteoSquarz@users.noreply.github.com>
2022-03-30 13:20:04 +0200

    Update Directory.py

--- Directory.py
+++ Directory.py
@@ -3,20 +3,7 @@ import os
 import string
 import random
 import psycopg2
-
-
-class Metodi:
-    def Login(pacchetto):
-        ipP2P = pacchetto[4:19]
-        ipP2P = ipP2P.replace("A", "")    #sostituisco 
-        pP2P = int(pacchetto[19:24])
-        #query = ""
-        #if(DB.queryDb(query)):
-        length_of_string = 16
-        SessionID = ''.join(random.SystemRandom().choice(string.digits) for _ in range(length_of_string))
-        #else:
-        #    SessionID = '0000000000000000'
-        return SessionID
+from datetime import datetime
 
 class DB:
     def queryDb(query):
@@ -25,9 +12,10 @@ class DB:
                                   password="admin",
                                   host="127.0.0.1",
                                   port="5432",
-                                  database="")
+                                  database="progettop2p")
             cursor = connection.cursor()
-            cursor.execute(query)
+            for i in range(len(query)):
+                cursor.execute(query[i])
             connection.commit()     #conferma e salva modifiche sul db
             if connection:
                 cursor.close()
@@ -39,11 +27,39 @@ class DB:
                 cursor.close()
                 connection.close()
                 return False
+
+class Metodi:
+    def Login(pacchetto):
+        ipP2P = pacchetto[4:19]
+        ipP2P = ipP2P.replace("A", "")    #sostituisco 
+        pP2P = pacchetto[19:24]
+        length_of_string = 16
+        SessionID = ''.join(random.SystemRandom().choice(string.digits) for _ in range(length_of_string))
+        query = []
+        query.append("INSERT INTO peer (ipp2p, pp2p, sessionid) VALUES('%s', '%s', '%s')" %(ipP2P, pP2P, SessionID))
+        data = str(datetime.today().strftime('%Y-%m-%d %H:%M'))
+        query.append("INSERT INTO log (ipp2p, pp2p, sessionid, operazione, data) VALUES('%s', '%s', '%s', 'login', '%s')" %(ipP2P, pP2P, SessionID, data))
+        if(DB.queryDb(query) != True):
+            SessionID = '0000000000000000'
+        return SessionID
+
+    def Aggiunta(pacchetto):
+        SessionID = pacchetto[4:20]
+        print(SessionID)
+        md5 = pacchetto[20:36]
+        print(md5)
+        filename = pacchetto[36:136].replace("Z", "")
+        print(filename)
+        return '005'
+
+
+
+
             
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("", 50002))
+s.bind(("", 50000))
 s.listen(10)
 
 while True:
@@ -56,6 +72,6 @@ while True:
         SessionID = Metodi.Login(pacchetto)
         risposta = 'ALGI' + SessionID
         conn.send(risposta.encode())
-
-
-    conn.close()
+    elif richiesta == "ADDF":
+        nCopie = Metodi.Aggiunta(pacchetto)
+        risposta = 'AADD' + nCopie
