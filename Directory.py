c85ad1a1ce95b163539d404f954a072f291b793e
MatteoSquarz <93669086+MatteoSquarz@users.noreply.github.com>
2022-03-30 17:11:36 +0200

    Update Directory.py

--- Directory.py
+++ Directory.py
@@ -8,25 +8,27 @@ from datetime import datetime
 class DB:
     def queryDb(query):
         try:
+            rowaffected = -1
             connection = psycopg2.connect(user="postgres",
                                   password="admin",
                                   host="127.0.0.1",
                                   port="5432",
                                   database="progettop2p")
             cursor = connection.cursor()
-            for i in range(len(query)):
-                cursor.execute(query[i])
+            cursor.execute(query)
+            rowaffected = cursor.rowcount
+            #print(str(rowaffected))
             connection.commit()     #conferma e salva modifiche sul db
             if connection:
                 cursor.close()
                 connection.close()
-                return True
+                return rowaffected    #ritorno numero righe coinvolte
         except (Exception, psycopg2.Error) as error:
             print("Error while fetching data from PostgreSQL", error)
             if connection:
                 cursor.close()
                 connection.close()
-                return False
+                return rowaffected
 
 class Metodi:
     def Login(pacchetto):
@@ -34,24 +36,37 @@ class Metodi:
         ipP2P = ipP2P.replace("A", "")    #sostituisco 
         pP2P = pacchetto[19:24]
         length_of_string = 16
-        SessionID = ''.join(random.SystemRandom().choice(string.digits) for _ in range(length_of_string))
+        SessionID = ''.join(random.SystemRandom().choice(string.digits) for _ in range(length_of_string))     #genero casualmente
         query = []
         query.append("INSERT INTO peer (ipp2p, pp2p, sessionid) VALUES('%s', '%s', '%s')" %(ipP2P, pP2P, SessionID))
         data = str(datetime.today().strftime('%Y-%m-%d %H:%M'))
         query.append("INSERT INTO log (ipp2p, pp2p, sessionid, operazione, data) VALUES('%s', '%s', '%s', 'login', '%s')" %(ipP2P, pP2P, SessionID, data))
-        if(DB.queryDb(query) != True):
-            SessionID = '0000000000000000'
+        for i in range(len(query)):
+            if(DB.queryDb(query[i]) != 1):       #se le query danno errori
+                SessionID = '0000000000000000'
         return SessionID
 
     def Aggiunta(pacchetto):
         SessionID = pacchetto[4:20]
-        print(SessionID)
-        md5 = pacchetto[20:36]
-        print(md5)
-        filename = pacchetto[36:136].replace("Z", "")
-        print(filename)
-        return '005'
-
+        #SessionID = '6338084801094733'
+        md5 = pacchetto[20:52]
+        filename = pacchetto[52:152].replace("|", "")
+        query = "select * from peer where sessionid = '%s'" %(SessionID)
+        if(DB.queryDb(query) == 1):     #se il sessionid è presente
+            query = "UPDATE file set nome = '%s' where ipp2p = (Select ipp2p from peer where sessionid = '%s') AND md5 = '%s'" %(filename, SessionID, md5)
+            if(DB.queryDb(query) == 0):    #se non esiste gia' un file con lo stesso identificativo md5 aggiunto da quel peer
+                query = "INSERT INTO file (nome, md5, ipp2p, pp2p) VALUES('%s', '%s', (Select ipp2p from peer where sessionid = '%s'), (Select pp2p from peer where sessionid = '%s'))" %(filename, md5, SessionID, SessionID)
+                if(DB.queryDb(query) == 1):    #se è stato inserito correttamente il file nel db
+                    query = "Select * from file where md5 = '%s'" %(md5)
+                    return str(DB.queryDb(query))    #ritorno numero copie con lo stesso identificativo md5
+                else:
+                    return 'err'
+            else:
+                query = "Select * from file where md5 = '%s'" %(md5)
+                return str(DB.queryDb(query))         #ritorno numero copie con lo stesso identificativo md5
+        #aggiungere query tabella di log qua
+        else:
+            return 'err'
 
 
 
@@ -64,14 +79,19 @@ s.listen(10)
 
 while True:
     conn, addr = s.accept()
-    pacchetto = conn.recv(4096).decode()
+    pacchetto = conn.recv(4096).decode()        
     richiesta = pacchetto[0:4]
-    print(pacchetto)
-    print(richiesta)
+    #print(pacchetto)
+    #print(richiesta)
     if richiesta == "LOGI":
         SessionID = Metodi.Login(pacchetto)
         risposta = 'ALGI' + SessionID
         conn.send(risposta.encode())
     elif richiesta == "ADDF":
         nCopie = Metodi.Aggiunta(pacchetto)
+        while(len(nCopie) < 3):
+            nCopie = "X" + nCopie
         risposta = 'AADD' + nCopie
+        #print(risposta)
+        conn.send(risposta.encode())
+    conn.close()
