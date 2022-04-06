6b753de80f4092e54b6db72c0b0d2a7fb04c155a
Andrea Piola <93667356+SigX@users.noreply.github.com>
2022-04-06 18:22:36 +0200

    Update Peer.py

--- Peer.py
+++ Peer.py
@@ -11,6 +11,7 @@ path = sys.argv[3]
 sessionId = '0000000000000000'
 nomi_File = []
 p = 0
+grandChunk = []
 
 def Menu():
     print('Menù di scelta:')
@@ -22,47 +23,72 @@ def Menu():
     print('6) Logout')
     return input('Cosa si desidera fare? ')
 
+def FindMd5(filename):
+    file = open('%s/%s' %(path,filename), 'rb')
+    contenuto = file.read()
+    file.close()
+    return str(hashlib.md5(contenuto).hexdigest())
+
 class Metodi:
     def Login(ip):
-            p = randint(50001,52000)
-            pacchetto = 'LOGI'+ip+ str(p) 
-            return pacchetto
+        p = randint(50001,52000)
+        pacchetto = 'LOGI'+ip+ str(p) 
+        return pacchetto
 
     def Aggiungi(sessionId, filename, path):   
-            file = open('%s/%s' %(path,filename), 'rb')
-            contenuto = file.read()
-            md5 = str(hashlib.md5(contenuto).hexdigest())
-            lunghezza = len(filename)
-            for i in range (100-lunghezza):
-                filename = '|' + filename
-            return ('ADDF'+sessionId+md5+filename)
+        md5 = FindMd5(filename)
+        lunghezza = len(filename)
+        for i in range (100-lunghezza):
+            filename = '|' + filename
+        return ('ADDF'+sessionId+md5+filename)
 
     def Rimuovi(sessionId, nomeFile):
-            file = open('%s/%s' %(path,nomeFile), 'rb')
-            contenuto = file.read()
-            md5 = str(hashlib.md5(contenuto).hexdigest())
-            pacchetto = 'DELF' + sessionId + md5
-            return pacchetto
+        file = open('%s/%s' %(path,nomeFile), 'rb')
+        contenuto = file.read()
+        file.close()
+        md5 = str(hashlib.md5(contenuto).hexdigest())
+        pacchetto = 'DELF' + sessionId + md5
+        return pacchetto
 
     def Ricerca(sessionId, ricerca):
-            lunghezza = len(ricerca)
-            for i in range (20-lunghezza):
-                ricerca = '|' + ricerca
-            pacchetto = 'FIND' + sessionId + ricerca
-            return pacchetto
+        lunghezza = len(ricerca)
+        for i in range (20-lunghezza):
+            ricerca = '|' + ricerca
+        pacchetto = 'FIND' + sessionId + ricerca
+        return pacchetto
 
-    def Download():
-        a = 1
-        """
-            while True:
-                buffer = s.recv(4096)
-                if not buffer: break 
-                else:
-                    risposta += buffer
-        """
+    def Download(md5):
+        pacchetto = "RETR" + md5
+        return pacchetto
 
     def Upload(md5):
-        a = 1
+        listMd5 = []
+        pacchetto = "ARET"
+        for file in nomi_File:
+            listMd5.append(FindMd5(file))
+        indice = listMd5.index(md5)
+        file = open('%s/%s' %(path,nomi_File[indice]), 'rb')
+        contenuto = file.read()
+        file.close()
+        modulo = len(contenuto) % 4096
+        if modulo > 0:
+            nChunk = int((len(contenuto)/4096)) + 1
+        else:
+            nChunk = int(len(contenuto)/4096)
+        pacchetto += str(nChunk)
+        chunk = []
+        i = 0
+        lunghezzaContenuto = len(contenuto)
+        while lunghezzaContenuto-4096 > 0:
+            chunk.append(contenuto[i:i+4096])
+            i += 4096
+            lunghezzaContenuto -= 4096
+            grandChunk.append("04096")
+        chunk.append(contenuto)
+        grandChunk.append(str(lunghezzaContenuto))
+        for j in range(len(chunk)):
+            pacchetto += grandChunk[j] + str(chunk[j]) 
+        return pacchetto
 
     def Logout(sessionId):
             pacchetto = 'LOGO' + sessionId
@@ -76,7 +102,7 @@ class L_File:
         self.pP2P = pP2P
 
 def CalcolaIp():
-    ipIn = '104.152.104.119'#s.getsockname()[0]
+    ipIn = '104.152.104.120'#s.getsockname()[0]
     split = ipIn.split('.')
     ip = ""
     for i in range (len(split)):
@@ -88,9 +114,9 @@ def CalcolaIp():
     return ip [0:15]
 
 
-def openSocketConnection():      
+def openSocketConnection(ip, port):      
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((hostname, int(porta)))
+    s.connect((ip, int(port)))
     return s
 
 def FiglioUpload():
@@ -106,7 +132,7 @@ def FiglioUpload():
             Metodi.Upload(fileMd5)
             sFiglio.close()
         
-def ScomponiRicerca(files):
+def ScomponiRicerca(files,risposta):
     k = 7
     for i in range(int(risposta[4:7])):
         md5 = risposta[k:k+32]
@@ -127,7 +153,7 @@ while True:
 
     if(selezione == '1'):
         if(sessionId == '0000000000000000'):
-            s = openSocketConnection()    #apro connessione con la socket
+            s = openSocketConnection(hostname, p)    #apro connessione con la socket
             pacchetto = Metodi.Login(CalcolaIp())
             s.send(pacchetto.encode())
             sessionId = s.recv(4096).decode()[4:20]
@@ -146,7 +172,7 @@ while True:
         if(sessionId != '0000000000000000'):       
             for filename in os.listdir(path):
                 nomi_File.append(filename)
-                s = openSocketConnection()     #apro connessione con la socket
+                s = openSocketConnection(hostname, p)     #apro connessione con la socket
                 pacchetto = Metodi.Aggiungi(sessionId, filename, path)
                 s.send(pacchetto.encode())
                 n_copie = s.recv(4096).decode()[4:7].replace("X", "")
@@ -160,7 +186,7 @@ while True:
         if(sessionId != '0000000000000000'):   
             nome_File = input('Inserire il nome del file da eliminare: ')    
             if (nome_File in nomi_File):                #CONTROLLARE FILE NON PRESENTI NELLA PROPRIA CARTELLA
-                s = openSocketConnection()     #apro connessione con la socket
+                s = openSocketConnection(hostname, p)     #apro connessione con la socket
                 pacchetto = Metodi.Rimuovi(sessionId, nome_File)
                 s.send(pacchetto.encode())
                 n_copie = s.recv(4096).decode()[4:7].replace("X", "")
@@ -177,7 +203,7 @@ while True:
             files = []
             ricerca = input('Inserire il nome del file da ricercare: ')
             if ricerca != "" and len(ricerca) <= 20:    
-                s = openSocketConnection()     #apro connessione con la socket
+                s = openSocketConnection(hostname, p)     #apro connessione con la socket
                 pacchetto = Metodi.Ricerca(sessionId, ricerca)
                 s.send(pacchetto.encode())
                 risposta = bytes(0)
@@ -192,7 +218,7 @@ while True:
                     print("La ricerca non ha prodotto risultati")
                 else:
                     #print(risposta)
-                    ScomponiRicerca(files)
+                    ScomponiRicerca(files, risposta)
                 for file in files:
                     print("Nome: %s || Md5: %s || ipP2P: %s || pP2P: %s" %(file.nome, file.md5, file.ipP2P, file.pP2P))
             else:
@@ -201,15 +227,28 @@ while True:
             print("È necessario prima fare il login")
 
 
-    elif(selezione == '5'):
-        if(sessionId != '0000000000000000'):
-
-        else:
-            print("È necessario prima fare il login")
+    elif(selezione == '5'):  
+        #if(sessionId != '0000000000000000'):
+            md5 = input("Inserire l'Md5 del file da scaricare")
+            ip = input("Inserire l'indirizzo ip del peer da cui scaricare il file")
+            port = input("Inserire porta del peer da cui scaricare il file")
+            s = openSocketConnection(ip, port)
+            pacchetto = "RETR" + md5
+            s.send(pacchetto.encode())
+            risposta = bytes(0)
+            while True:
+                buffer = s.recv(4096)
+                if not buffer: break 
+                else:
+                    risposta += buffer
+            risposta = risposta.decode()
+            print(risposta)
+        #else:
+            #print("È necessario prima fare il login")
 
     elif(selezione == '6'):
         if(sessionId != '0000000000000000'):
-            s = openSocketConnection() 
+            s = openSocketConnection(hostname, p) 
             pacchetto = Metodi.Logout(sessionId)
             s.send(pacchetto.encode())
             n_file = s.recv(4096).decode()[4:7].replace('X','')
